e9172487dd55655578567e684820e39728358d33
Jody Castor <jdcastor@gmail.com>
2022-03-01 16:55:42 -0600

    initial setup

---
+++ snippets/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Snippet
+
+@admin.register(Snippet)
+class RequestDemoAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Snippet._meta.get_fields()]
