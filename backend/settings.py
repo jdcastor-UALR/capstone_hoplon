b5d809dea7ad33053022c9c97e5a5ee88823e3dc
Ethan O'Leske <edoleske@ualr.edu>
2022-03-02 21:59:26 -0600

    Adding ssl_require parameter to database URL parse

--- backend/settings.py
+++ backend/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=600)
+db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
