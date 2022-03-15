881bb29ae94a68c03a92d832e6ed05c406e1f78c
Jmteem <70149287+Jmteem@users.noreply.github.com>
2022-03-15 16:08:13 -0500

    Refactor snippets to Django_API for clarification.

--- Django_API/serializers.py
+++ Django_API/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from Django_API.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
