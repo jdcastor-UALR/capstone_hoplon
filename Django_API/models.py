88e09233b8c2e7c4bbfbc3af2d2b49b6873636cf
Jmteem <70149287+Jmteem@users.noreply.github.com>
2022-03-17 11:16:47 -0500

    Retrying models commit

--- Django_API/models.py
+++ Django_API/models.py
@@ -9,6 +9,66 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
 
+
+class User(models.Model):
+    name = models.CharField(max_length=30, blank=False, default='')
+    password = models.CharField(max_length=30, blank=False, default='')
+    email = models.EmailField(max_length=30, blank=False, default='')
+    # AccessLevel
+
+
+class RegistrationRequest(models.Model):
+    Root = 'Root'
+    Admin = 'Admin'
+    Assistant = 'Assistant'
+    Public = 'Public'
+
+    ACCESS_LEVEL_CHOICES = [
+        (Root, 'Root'),
+        (Admin, 'Admin'),
+        (Assistant, 'Assistant'),
+        (Public, 'Public')
+    ]
+
+    access_level = models.CharField(
+        max_length=9,
+        choices=ACCESS_LEVEL_CHOICES,
+        default=Public,
+        blank=False
+    )
+
+    requested_user_name = models.CharField(max_length=30, blank=False, default='')
+    requested_password = models.CharField(max_length=30, blank=False, default='')
+    contact_email = models.EmailField(max_length=30, blank=False, default='')
+
+
+class Session(models.Model):
+    # Id = Session ID, which is automatically generated
+    associated_user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class TimeSlot(models.Model):
+    begin_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+
+class Course(models.Model):
+    course_title = models.CharField(max_length=30, blank=False, default='')
+    course_number = models.CharField(max_length=4, blank=False, default='')
+    subject_disciplines = models.CharField(max_length=100, blank=False, default='')
+
+
+class Section(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    meetingTimes = models.ForeignKey(TimeSlot, on_delete=models.CASCADE)
+
+
+class Instructor(models.Model):
+    lastName = models.CharField(max_length=30, blank=False, default='')
+    maxSections = models.IntegerField(max_length=2, blank=False, default='1')
+    # qualifications = models.Discipline
+
+
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
@@ -17,5 +77,6 @@ class Snippet(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
+
     class Meta:
-        ordering =['created']
+        ordering = ['created']
