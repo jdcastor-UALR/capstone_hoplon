8e559bb238404c8186bd958a243358dc4547d91a
Ethan O'Leske <edoleske@ualr.edu>
2022-03-18 13:48:27 -0500

    Designing frontend pages with dummy data

--- src/components/AssistantPage/AssistantPage.js
+++ src/components/AssistantPage/AssistantPage.js
@@ -1,11 +1,91 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Typography from "@material-ui/core/Typography";
+import useTheme from "@material-ui/core/styles/useTheme";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import EditIcon from "@material-ui/icons/Edit"
 
-const AssistantPage = () => (
-  <div data-testid="AssistantPage">
-    AssistantPage Component
-  </div>
-);
+const generateCards = (schedule) => {
+  let cards = [];
+  let count = 1;
+  for (let option of schedule) {
+    cards.push(
+      <Card variant={"outlined"} key={option.id} style={{width: '20%', margin: '1em'}}>
+        <CardContent>
+          <Typography>Option {count}</Typography>
+          <Typography>Covered Classes: {option.assignments.filter(assignment => assignment.instructor != null).length}
+            /{option.assignments.length}
+          </Typography>
+          <Typography>Issues: {(option.issues.length > 0) ? option.issues.join(', ') : 'None'}</Typography>
+        </CardContent>
+        <CardActions>
+          <IconButton style={{marginLeft: "auto"}}>
+            <EditIcon />
+          </IconButton>
+        </CardActions>
+      </Card>
+    );
+    count += 1;
+  }
+  return cards;
+}
+
+const AssistantPage = () => {
+  const theme = useTheme();
+
+  const dummySchedule = [
+    {assignments: [
+        {section: 'Section1', instructor: 'Smith'},
+        {section: 'Section2', instructor: 'Jones'},
+        {section: 'Section3', instructor: 'Smith'},
+        {section: 'Section4', instructor: 'Levett'},
+        {section: 'Section5', instructor: 'Levett'},
+      ], issues: ['Teacher teaching too many classes', 'Teacher teaching outside of disciplines'], id: 1},
+    {assignments: [
+        {section: 'Section1', instructor: 'Smith'},
+        {section: 'Section2', instructor: 'Jones'},
+        {section: 'Section3', instructor: 'Smith'},
+        {section: 'Section4', instructor: 'Levett'},
+        {section: 'Section5', instructor: null},
+      ], issues: ['Teacher teaching too many classes'], id: 2},
+    {assignments: [
+        {section: 'Section1', instructor: 'Smith'},
+        {section: 'Section2', instructor: 'Jones'},
+        {section: 'Section3', instructor: null},
+        {section: 'Section4', instructor: 'Levett'},
+        {section: 'Section5', instructor: null},
+      ], issues: ['Teacher teaching too many classes'], id: 3},
+    {assignments: [
+        {section: 'Section1', instructor: 'Smith'},
+        {section: 'Section2', instructor: 'Jones'},
+        {section: 'Section3', instructor: null},
+        {section: 'Section4', instructor: null},
+        {section: 'Section5', instructor: null},
+      ], issues: [], id: 4},
+    {assignments: [
+        {section: 'Section1', instructor: 'Smith'},
+        {section: 'Section2', instructor: null},
+        {section: 'Section3', instructor: null},
+        {section: 'Section4', instructor: null},
+        {section: 'Section5', instructor: null},
+      ], issues: [], id: 5},
+  ];
+
+  return (
+    <div data-testid="AssistantPage">
+      <Typography variant="h3" style={{color: theme.palette.primary, fontWeight: "bold"}}>
+        Generated Schedules
+      </Typography>
+      <Grid container alignItems={"center"} justifyContent={"center"}>
+        {generateCards(dummySchedule)}
+      </Grid>
+    </div>
+  );
+}
 
 AssistantPage.propTypes = {};
 
--- src/components/SetupPage/SetupPage.js
+++ src/components/SetupPage/SetupPage.js
@@ -1,11 +1,111 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Grid from "@material-ui/core/Grid";
+import Divider from "@material-ui/core/Divider";
+import useTheme from "@material-ui/core/styles/useTheme";
+import Typography from "@material-ui/core/Typography";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import EditIcon from "@material-ui/icons/Edit"
+import AddIcon from "@material-ui/icons/Add"
+import IconButton from "@material-ui/core/IconButton";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 
-const SetupPage = () => (
-  <div data-testid="SetupPage">
-    SetupPage Component
-  </div>
-);
+const InstructorList = (instructors) => {
+  let listItems = [];
+
+  for (let instructor of instructors) {
+    listItems.push(
+      <ListItem>
+        <ListItemText primary={instructor.lastName}
+                      secondary={'Assignment Limit: ' + instructor.maxSections.toString()} />
+        <ListItemSecondaryAction>
+          <IconButton edge={"end"} aria-label={"edit"}>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+    listItems.push(<Divider />);
+  }
+
+  return listItems;
+};
+
+const ClassList = (classes) => {
+  let listItems = [];
+
+  for (let cls of classes) {
+    listItems.push(
+      <ListItem>
+        <ListItemText primary={cls.courseTitle} secondary={'CPSC ' + cls.courseNumber + ' - ' + cls.meetingTimes} />
+        <ListItemSecondaryAction>
+          <IconButton edge={"end"} aria-label={"edit"}>
+            <EditIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+    );
+    listItems.push(<Divider />);
+  }
+
+  return listItems;
+};
+
+const SetupPage = () => {
+  const theme = useTheme();
+
+  const dummyInstructors = [
+    {lastName: 'Smith', maxSections: 5, qualifications: ['Programming', 'Data Structures']},
+    {lastName: 'Smith', maxSections: 5, qualifications: ['Programming', 'Data Structures']},
+    {lastName: 'Smith', maxSections: 5, qualifications: ['Programming', 'Data Structures']},
+  ];
+
+  const dummyClasses = [
+    {courseTitle: 'Programming 1', courseNumber: 1135, subjectDisciplines: ['Programming', 'Algorithms'],
+      meetingTimes: 'MW 9:25 AM'},
+    {courseTitle: 'Programming 1', courseNumber: 1135, subjectDisciplines: ['Programming', 'Algorithms'],
+      meetingTimes: 'TR 9:25 AM'},
+  ];
+
+  return (
+    <div data-testid="SetupPage">
+      <Grid container spacing={2} justifyContent={"center"}>
+        <Grid container item xs={5} direction={"column"}>
+          <Typography variant={"h4"} style={{color: theme.palette.primary, fontWeight: "bold"}}>
+            Teaching Staff
+          </Typography>
+          <div style={{padding: '1rem 5rem'}}>
+            <List style={{border: `1px #0000001f solid`}}>
+              {InstructorList(dummyInstructors)}
+              <ListItem button>
+                <ListItemIcon><AddIcon /></ListItemIcon>
+                <ListItemText primary={'Add New'} />
+              </ListItem>
+            </List>
+          </div>
+        </Grid>
+        <Divider orientation={"vertical"} flexItem />
+        <Grid container item xs={5} direction={"column"}>
+          <Typography variant={"h4"} style={{color: theme.palette.primary, fontWeight: "bold"}}>
+            Class Roster
+          </Typography>
+          <div style={{padding: '1rem 5rem'}}>
+            <List style={{border: `1px #0000001f solid`}}>
+              {ClassList(dummyClasses)}
+              <ListItem button>
+                <ListItemIcon><AddIcon /></ListItemIcon>
+                <ListItemText primary={'Add New'} />
+              </ListItem>
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
 SetupPage.propTypes = {};
 
