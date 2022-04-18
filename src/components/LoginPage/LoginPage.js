1abfad1e9cdcbca1f2d963458dd3d470bdb28b2d
Ethan O'Leske <edoleske@ualr.edu>
2022-04-18 11:42:08 -0500

    Adding registration request form

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -5,15 +5,9 @@ import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
-const LoginPage = () => {
-  const theme = useTheme();
-
-
+const LoginForm = () => {
   return (
-    <div data-testid="LoginPage">
-      <Typography variant="h3" style={{color: theme.palette.primary, fontWeight: "bold", marginBottom: "0.5em"}}>
-        Login
-      </Typography>
+    <>
       <Grid container alignItems={"center"} justifyContent={"center"} spacing={2}>
         <Grid item xs={12}>
           <TextField required label={"Email"} type={"text"} />
@@ -22,7 +16,42 @@ const LoginPage = () => {
           <TextField required label={"Password"} type={"password"} />
         </Grid>
       </Grid>
-      <Button color={"primary"} variant={"contained"} style={{marginTop: '2em'}}>Submit</Button>
+    </>
+  );
+};
+
+const RegisterForm = () => {
+  return (
+    <>
+      <Grid container alignItems={"center"} justifyContent={"center"} spacing={2}>
+        <Grid item xs={12}>
+          <TextField required label={"Email"} type={"text"} />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField required label={"Requested Password"} type={"password"} />
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+const LoginPage = () => {
+  const theme = useTheme();
+
+  const [isLoginForm, setIsLoginForm] = useState(true);
+
+  return (
+    <div data-testid="LoginPage">
+      <Typography variant="h3" style={{color: theme.palette.primary, fontWeight: "bold", marginBottom: "0.5em"}}>
+        {(isLoginForm) ? 'Login' : 'Submit Registration Request'}
+      </Typography>
+      {(isLoginForm) ? LoginForm() : RegisterForm()}
+      <Button color={"primary"} variant={"contained"} style={{marginTop: '2em', marginRight: '1em'}}>Submit</Button>
+      <Button color={"default"} variant={"contained"}
+              style={{marginTop: '2em'}}
+              onClick={() => setIsLoginForm(!isLoginForm)}>
+        {(isLoginForm) ? 'Register' : 'Return to Login'}
+      </Button>
     </div>
   );
 };
