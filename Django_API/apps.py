95711d510fec1941c652ed437a7b74a6106cc5a7
Ethan O'Leske <edoleske@ualr.edu>
2022-05-01 23:08:57 -0500

    Undo of comment out

--- Django_API/apps.py
+++ Django_API/apps.py
@@ -7,45 +7,45 @@ import logging
 logger = logging.getLogger()
 
 
-# class djangoApiConfig(AppConfig):
-#     default_auto_field = 'django.db.models.BigAutoField'
-#     name = 'Django_API'
-#
-#     @staticmethod
-#     def db_initialized():
-#         from .models import DBInitStatus
-#         try:
-#             if DBInitStatus.objects.count() == 0:
-#                 DBInitStatus.objects.create(root_initialized=False)
-#             return DBInitStatus.objects.get(id=1).root_initialized
-#         except ProgrammingError as pe:
-#             logger.error(pe)
-#
-#     @staticmethod
-#     def mark_db_initialized():
-#         from .models import DBInitStatus
-#         try:
-#             initialized = DBInitStatus.objects.get(id=1)
-#             initialized.root_initialized = True
-#             initialized.save()
-#         except ProgrammingError as pe:
-#             logger.error(pe)
-#
-#     def ready(self):
-#         from .models import User
-#         if not djangoApiConfig.db_initialized():
-#             try:
-#                 try:
-#                     root = User.objects.create_superuser(username="root",
-#                                                          password="root"
-#                                                          )
-#                     root.accessLevel = "root"
-#                     root.is_active = False
-#                     root.save()
-#                     logger.info(msg="Created initial inactive root user with password root")
-#                 except IntegrityError as ie:
-#                     logger.error(ie)
-#                 finally:
-#                     djangoApiConfig.mark_db_initialized()
-#             except ProgrammingError as pe:
-#                 logger.error(pe)
+class djangoApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Django_API'
+
+    @staticmethod
+    def db_initialized():
+        from .models import DBInitStatus
+        try:
+            if DBInitStatus.objects.count() == 0:
+                DBInitStatus.objects.create(root_initialized=False)
+            return DBInitStatus.objects.get(id=1).root_initialized
+        except ProgrammingError as pe:
+            logger.error(pe)
+
+    @staticmethod
+    def mark_db_initialized():
+        from .models import DBInitStatus
+        try:
+            initialized = DBInitStatus.objects.get(id=1)
+            initialized.root_initialized = True
+            initialized.save()
+        except ProgrammingError as pe:
+            logger.error(pe)
+
+    def ready(self):
+        from .models import User
+        if not djangoApiConfig.db_initialized():
+            try:
+                try:
+                    root = User.objects.create_superuser(username="root",
+                                                         password="root"
+                                                         )
+                    root.accessLevel = "root"
+                    root.is_active = False
+                    root.save()
+                    logger.info(msg="Created initial inactive root user with password root")
+                except IntegrityError as ie:
+                    logger.error(ie)
+                finally:
+                    djangoApiConfig.mark_db_initialized()
+            except ProgrammingError as pe:
+                logger.error(pe)
